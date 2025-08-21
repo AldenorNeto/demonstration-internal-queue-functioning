ff80e4cb94db6e4fb4a2839992f052dc48aed4ea
Aldenor Neto 🦁 <aldenor.code@gmail.com>
2025-08-21 18:25:47 -0300

    refactor: remove QueueMonitorService from WorkerModule

--- apps/worker/src/worker.module.ts
+++ apps/worker/src/worker.module.ts
@@ -2,10 +2,9 @@ import { CALC_QUEUE, SharedModule } from '@app/shared';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { FibonacciCalcProcessor } from './processors/fibonacci-calc.processor';
-import { QueueMonitorService } from './queue-monitor.service';
 
 @Module({
   imports: [SharedModule, BullModule.registerQueue({ name: CALC_QUEUE })],
-  providers: [FibonacciCalcProcessor, QueueMonitorService],
+  providers: [FibonacciCalcProcessor],
 })
 export class WorkerModule {}
